e2319d7b4fd78abc706afc730c8326e24889eab1
rizkyrachman00 <rachman.ti2021@students.amikom.ac.id>
2024-08-06 13:54:50 +0700

    error file route.ts on folder liveblocks-auth

---
+++ lib/actions/room.actions.ts
@@ -0,0 +1,11 @@
+import { nanoid } from "nanoid";
+
+export const createDocument = async ({
+  userId,
+  email,
+}: CreateDocumentParams) => {
+  const roomId = nanoid();
+
+  try {
+  } catch (error) {}
+};
